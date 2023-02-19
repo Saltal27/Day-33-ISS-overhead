f93023e8d9d95cb2a9e81dc29dbc474fb9476b63
Saltal27 <omarmobarak53@gmail.com>
2023-02-19 11:21:19 +0300

    final

--- main.py
+++ main.py
@@ -5,53 +5,41 @@ import smtplib
 MY_LAT = 33.510414
 MY_LONG = 36.278336
 
-response = requests.get(url="http://api.open-notify.org/iss-now.json")
-response.raise_for_status()
-data = response.json()
-iss_latitude = float(data["iss_position"]["latitude"])
-iss_longitude = float(data["iss_position"]["longitude"])
-
-parameters = {
-    "lat": MY_LAT,
-    "lng": MY_LONG,
-    "formatted": 0,
-}
-
-response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
-response.raise_for_status()
-data = response.json()
-sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
-sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
-
-time_now = datetime.now()
-this_hour = time_now.hour
-this_minute = time_now.minute
-
 
 def check_pos():
+    response = requests.get(url="http://api.open-notify.org/iss-now.json")
+    response.raise_for_status()
+    data = response.json()
+    iss_latitude = float(data["iss_position"]["latitude"])
+    iss_longitude = float(data["iss_position"]["longitude"])
+
     if MY_LONG - 5 <= iss_longitude <= MY_LONG + 5 and MY_LAT - 5 <= iss_latitude <= MY_LAT + 5:
         return True
 
 
 def check_dark():
+    time_now = datetime.now()
+    this_hour = time_now.hour
+
+    parameters = {
+        "lat": MY_LAT,
+        "lng": MY_LONG,
+        "formatted": 0,
+    }
+    response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
+    response.raise_for_status()
+    data = response.json()
+    sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
+    sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
+
     if this_hour < sunrise or this_hour > sunset:
         return True
 
 
-# if check_pos() and check_dark():
-#     print("hi")
-# elif check_pos():
-#     print("shy")
-# elif check_dark():
-#     print("shi")
-# else:
-#     print("adios")
-
-my_email = "pythontest32288@gmail.com"
-my_password = "gsrfzucledwimgqp"
-
-
 def send_iss_mail():
+    my_email = "pythontest32288@gmail.com"
+    my_password = "gsrfzucledwimgqp"
+
     with smtplib.SMTP("smtp.gmail.com") as connection:
         connection.starttls()
         connection.login(user=my_email, password=my_password)
@@ -66,6 +54,9 @@ minute_check = 0
 
 def keep_checking():
     global minute_check
+
+    time_now = datetime.now()
+    this_minute = time_now.minute
 
     if this_minute != minute_check:
         if check_dark() and check_pos():
