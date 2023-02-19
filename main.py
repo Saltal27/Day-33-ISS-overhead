d6576924bb0f81e085e0c4bad5aa7d9b1f8a96c4
Saltal27 <omarmobarak53@gmail.com>
2023-02-19 11:10:13 +0300

    final

--- main.py
+++ main.py
@@ -1,18 +1,15 @@
 import requests
 from datetime import datetime
+import smtplib
 
-MY_LAT = 51.507351 # Your latitude
-MY_LONG = -0.127758 # Your longitude
+MY_LAT = 33.510414
+MY_LONG = 36.278336
 
 response = requests.get(url="http://api.open-notify.org/iss-now.json")
 response.raise_for_status()
 data = response.json()
-
 iss_latitude = float(data["iss_position"]["latitude"])
 iss_longitude = float(data["iss_position"]["longitude"])
-
-#Your position is within +5 or -5 degrees of the ISS position.
-
 
 parameters = {
     "lat": MY_LAT,
@@ -27,11 +24,51 @@ sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
 sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
 
 time_now = datetime.now()
-
-#If the ISS is close to my current position
-# and it is currently dark
-# Then send me an email to tell me to look up.
-# BONUS: run the code every 60 seconds.
+this_hour = time_now.hour
+this_minute = time_now.minute
 
 
+def check_pos():
+    if MY_LONG - 5 <= iss_longitude <= MY_LONG + 5 and MY_LAT - 5 <= iss_latitude <= MY_LAT + 5:
+        return True
 
+
+def check_dark():
+    if this_hour < sunrise or this_hour > sunset:
+        return True
+
+
+# if check_pos() and check_dark():
+#     print("hi")
+# elif check_pos():
+#     print("shy")
+# elif check_dark():
+#     print("shi")
+# else:
+#     print("adios")
+
+my_email = "pythontest32288@gmail.com"
+my_password = "gsrfzucledwimgqp"
+
+
+def send_iss_mail():
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(user=my_email, password=my_password)
+        connection.sendmail(from_addr=my_email,
+                            to_addrs="omarmobarak53@gmail.com",
+                            msg="Subject:ISS!\n\nLook up!\nYou can see the ISS in the sky right now"
+                            )
+
+
+minute_check = 0
+
+
+def keep_checking():
+    global minute_check
+
+    if this_minute != minute_check:
+        if check_dark() and check_pos():
+            send_iss_mail()
+        minute_check = this_minute
+    keep_checking()
